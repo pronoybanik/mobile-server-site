64d0404a6bd1d3598c0ce60a3f0def12d43ca0c4
PRONOY <pronoybanik82@gmail.com>
2022-12-15 11:58:09 +0600

    jwt works

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const cors = require('cors')
 const app = express();
+const jwt = require('jsonwebtoken')
 require('dotenv').config();
 const port = process.env.POST || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+
 
 app.use(cors());
 app.use(express.json());
@@ -18,6 +21,7 @@ async function run() {
         const productCategoriesCollection = client.db('mobileSite').collection('productCategories');
         const productCollection = client.db('mobileSite').collection('products');
         const ordersCollection = client.db('mobileSite').collection('orders');
+        const userCollection = client.db('mobileSite').collection('user');
 
 
         app.get('/categories', async (req, res) => {
@@ -26,7 +30,7 @@ async function run() {
             // console.log(result);
             res.send(result)
 
-        })
+        });
 
         app.get('/products/:id', async (req, res) => {
             const id = req.params.id;
@@ -35,34 +39,61 @@ async function run() {
             // console.log(products);
             res.send(products)
 
-        })
+        });
 
         app.get('/productsDetails/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) }
             const products = await productCollection.findOne(query)
             res.send(products)
-        })
+        });
+
+        // product post API
 
         app.post('/booking', async (req, res) => {
             const id = req.body;
             const result = await ordersCollection.insertOne(id);
             res.send(result)
-        })
+        });
+ 
+        // product GET API
 
         app.get('/booking', async (req, res) => {
             const query = {}
             const orders = await ordersCollection.find(query).toArray()
             res.send(orders)
 
-        })
+        });
 
         app.delete('/booking/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) }
             const bookingId = await ordersCollection.deleteOne(query)
             res.send(bookingId)
+        });
+
+        // jwt token create
+
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email;
+            // console.log(email);
+            const query = { email: email }
+            const user = await userCollection.findOne(query);
+            if (user) {
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '24h' })
+                return res.send({ accessToken: token })
+            }
+            // console.log(user);
+            res.status(403).send({ accessToken: '' })
         })
+
+        // add all user 
+        app.post('/user', async (req, res) => {
+            const id = req.body;
+            const users = await userCollection.insertOne(id);
+            res.send(users);
+        });
+
 
 
 
